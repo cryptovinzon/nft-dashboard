46ad7db50f5e4294dd7f106434d677e95170e962
Vinzon <luongvinson@gmail.com>
2022-07-16 21:05:16 -0600

    revised time to show hours

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function displayListings(listings){
         let div = document.createElement('div');
         let img = document.createElement('img');
         let a = document.createElement('a');
-        let time = getMinutesAgo(i);
+        let time = getMinutesAgo(listings, i);
 
         a.href = `https://illuvidex.illuvium.io/asset/0x9e0d99b864e1ac12565125c5a82b59adea5a09cd/${listings.result[i].sell.data.token_id}`;
 
@@ -34,7 +34,7 @@ function displayListings(listings){
         div.textContent = 
             `${listings.result[i].sell.data.properties.name}
             ${listings.result[i].buy.data.quantity/(10**(listings.result[i].buy.data.decimals))}
-            ${listings.result[i].buy.type} ${time} min ago`;
+            ${listings.result[i].buy.type} ${time}`;
 
         listingsContainer.appendChild(a).appendChild(div).appendChild(img);                               
     }
@@ -47,6 +47,7 @@ function displaySales(sales){
         let div = document.createElement('div');
         let img = document.createElement('img');
         let a = document.createElement('a');
+        let time = getMinutesAgo(sales, i)
         a.href = `https://illuvidex.illuvium.io/asset/0x9e0d99b864e1ac12565125c5a82b59adea5a09cd/${sales.result[i].buy.data.token_id}`;
 
         div.classList.add('item-container');
@@ -55,7 +56,7 @@ function displaySales(sales){
         div.textContent = 
             `${sales.result[i].buy.data.properties.name}
             ${sales.result[i].sell.data.quantity_with_fees/(10**(sales.result[i].sell.data.decimals))}
-            ${sales.result[i].sell.type} `;
+            ${sales.result[i].sell.type} ${time}`;
 
         salesContainer.appendChild(a).appendChild(div).appendChild(img);                               
     }      
@@ -74,7 +75,7 @@ function showListingSummary() {
         let name = listings.result[i].sell.data.properties.name;
         let price = listings.result[i].buy.data.quantity/(10**listings.result[i].buy.data.decimals);
         let currency = listings.result[i].buy.type;
-        let time = getMinutesAgo(i) + 'min ago';
+        let time = getMinutesAgo(listings, i);
 
         nameDiv.textContent = name;
         priceDiv.textContent = price;
@@ -85,10 +86,14 @@ function showListingSummary() {
     }
 }
 
-function getMinutesAgo(index) {
+function getMinutesAgo(array, index) {
     let currentTime = new Date();
-    let listTime = new Date (listings.result[index].timestamp);
-    return Math.round((currentTime - listTime)/(1000 * 60));
+    let listTime = new Date (array.result[index].timestamp);
+    if (Math.round((currentTime - listTime)/(1000 * 60)) < 60) {
+        return Math.round((currentTime - listTime)/(1000 * 60)) + ' min ago';
+    } else {
+        return Math.round((currentTime - listTime)/(1000 * 60)/60) + ' h ago';
+    }    
 }
 
 // learn to return into div without storying in array
